5ceb2199a980cac4ff501bd8507dbb182053ed8b
Kelci Huang <kelci.huang@gmail.com>
2019-07-05 15:38:39 -0600

    add ios resources

--- FamilyNoteApp/FamilyNoteApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ FamilyNoteApp/FamilyNoteApp.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "91cdba35-de47-4b1e-9447-7f6a12952398",
+		"XAMARIN_BUILD_ID", "703ad4b1-ed4e-4f92-8ff4-5a6fbe50ecad",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
